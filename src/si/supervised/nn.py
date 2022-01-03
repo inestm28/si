43db1aae617b2351f8aa3154e041ffb9008e1e7a
inestm28 <pg44378@alunos.uminho.pt>
2022-01-03 09:39:36 +0000

    4a_eval

--- src/si/supervised/nn.py
+++ src/si/supervised/nn.py
@@ -1,5 +1,117 @@
-class NN:
+import numpy as np
+from .model import Model
+from abc import ABC, abstractmethod
+from si.util.metrics import mse, mse_prime
+# from scipy import signal
 
-class Dense:
+class Layer(ABC):
+    def __init__(self):
+        self.input = None
+        self.output = None
 
-class Activation:
+    @abstractmethod
+    def forward(self, input):
+        raise NotImplementedError
+
+    @abstractmethod
+    def backward(self, output_error, lr):
+        #lr is the learning rate (alpha)
+        raise NotImplementedError
+
+class Dense(Layer):
+    #Fully Connected layer
+    def __init__(self, input_size, output_size):
+        self.weights= np.random.rand(input_size, output_size) -0.5
+        self.bias= np.zeros((1, output_size))
+
+    def setWeights(self, weights, bias):
+        #Sets the weight and the bias for the NN
+        if (weights.shape != self.weights.shape):
+            raise ValueError(f'shapes mismatch {weights.shape} and {self.weights.shape}") ')
+        if (bias.shape != self.bias.shape):
+            raise ValueError(f"Shapes missmatch{bias.shape}and {self.bias.shape}")
+        self.weights = weights
+        self.bias = bias
+
+    def forward(self, input):
+        self.input = input
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+
+    def backward(self, output_error, lr):
+        'computes dE/dW, dE/dB for a given output_error=dE/dY'
+        # compute the weights error dE/dW = X.T*dE/dY
+        weights_error = np.dot(self.input.T, output_error)
+        # compute the bias error dE/dB
+        bias_error = np.sum(output_error, axis=0)
+        input_error = np.dot(output_error, self.weights.T)
+        # update parameters
+        self.weights -= lr * weights_error
+        self.bias -= lr * bias_error
+        return input_error
+
+class Activation(Layer):
+    def __init__(self, activation):
+        self.activation = activation
+
+    def forward(self, input):
+        self.input = input
+        self.output = self.activation(self.input)
+        return self.output
+
+    def backward(self, output_error, lr):
+        # learning rate is not used because there is no "learnable" parameters
+        # only passes the error do the previous layer
+        return np.multiply(self.activation.prime(self.input), output_error)
+
+
+class NN(Model):
+    def __init__(self, epochs=1000, lr=0.001, verbose=True):
+        #neural network model
+        self.epochs = epochs
+        self.lr = lr
+        self.verbose = verbose
+        self.layers = []
+        self.loss = mse
+        self.loss_prime = mse_prime
+
+    def add(self, layer):
+        self.layers.append(layer)
+
+    def fit(self, dataset):
+        X, y = dataset.getXy()
+        self.dataset = dataset
+        self.history = dict()
+        for epoch in range(self.epochs):
+            output = X
+            # forward propagation
+            for layer in self.layers:
+                output = layer.forward(output)
+
+            # backward propagation
+            error = self.loss_prime(y, output)
+            for layer in reversed(self.layers):
+                error = layer.backward(error, self.lr)
+
+            # calculate average error on all samples
+            err = self.loss(y, output)
+            self.history[epoch] = err
+            if self.verbose:
+                print(f'epoch {epoch+1}/{self.epochs} error = {err}')
+        if not self.verbose:
+            print(f"error={err}")
+        self.is_fitted = True
+
+    def predict(self, input):
+        assert self.is_fitted, 'Model must be fit before'
+        output = input
+        for layer in self.layers:
+            output = layer.forward(output)
+        return output
+
+    def cost(self, X=None, y=None):
+        assert self.is_fitted, 'Model must be fit'
+        X = X if X is not None else self.dataset.X
+        y = y if y is not None else self.dataset.y
+        output = self.predict(X)
+        return self.loss(y, output)
