89fb23baae587598326b34561c6d3389a2ce8d8a
inestm28 <pg44378@alunos.uminho.pt>
2022-01-17 17:13:19 +0000

    eval5

--- src/si/supervised/nn.py
+++ src/si/supervised/nn.py
@@ -9,7 +9,7 @@ from .model import Model
 #from scipy import signal
 
 from si.util.metrics import mse, mse_prime
-from si.util.im2col import pad2D, im2col, col2im
+# from si.util.im2col import pad2D, im2col, col2im
 
 __all__ = ['Dense', 'Activation', 'NN']
 
@@ -124,83 +124,6 @@ class NN(Model):
         y = y if y is not None else self.dataset.y
         output = self.predict(X)
         return self.loss(y, output)
-
-
-class Flatten(Layer):
-    def forward(self, input):
-        self.input_shape = input.shape
-        output = input.reshape(input.shape[0], -1)
-        return output
-
-    def backward(self, output_error, lr):
-        return output_error.reshape(self.input_shape)
-
-
-class Conv2D(Layer):
-    def __init__(self, input_shape, kernel_shape, layer_depth, stride=1, padding=0):
-        self.input_shape = input_shape
-        self.in_ch = input_shape[2]
-        self.out_ch = layer_depth
-        self.stride = stride
-        self.padding = padding
-        self.weights = np.random.rand(kernel_shape[0], kernel_shape[1],
-                                      self.in_ch, self.out_ch) - 0.5
-
-        self.bias = np.zeros((self.out_ch, 1))
-
-    def forward(self, input_data):
-        s = self.stride
-        self.X_shape = input_data.shape
-        _, p = pad2D(input_data, self.padding, self.weights.shape[:2], s)
-
-        pr1, pr2, pc1, pc2 = p
-        fr, fc, in_ch, out_ch = self.weights.shape
-        n_ex, in_rows, in_cols, in_ch = input_data.shape
-
-        # compute the dimensions of the convolution output
-        out_rows = int((in_rows + pr1 + pr2 - fr) / s + 1)
-        out_cols = int((in_cols + pc1 + pc2 - fc) / s + 1)
-
-        # convert X and w into the appropriate 2D matrices and take their product
-        self.X_col, _ = im2col(input_data, self.weights.shape, p, s)
-        W_col = self.weights.transpose(3, 2, 0, 1).reshape(out_ch, -1)
-
-        output_data = (W_col @ self.X_col + self.bias).reshape(out_ch, out_rows, out_cols, n_ex).transpose(3, 1, 2, 0)
-        return output_data
-
-    def backward(self, output_error, learning_rate):
-        fr, fc, in_ch, out_ch = self.weights.shape
-        p = self.padding
-        db = np.sum(output_error, axis=(0, 1, 2))
-        db = db.reshape(out_ch, )
-        dout_reshaped = output_error.transpose(1, 2, 3, 0).reshape(out_ch, -1)
-        dW = dout_reshaped @ self.X_col.T
-        dW = dW.reshape(self.weights.shape)
-
-        W_reshape = self.weights.reshape(out_ch, -1)
-        dX_col = W_reshape.T @ dout_reshaped
-        input_error = col2im(dX_col, self.X_shape, self.weights.shape, (p, p, p, p), self.stride)
-
-        self.weights -= learning_rate * dW
-        self.bias -= learning_rate * db
-        return input_error
-
-    def predict(self, input_data):
-        assert self.is_fitted, 'Model must be fit before predicting'
-        output = input_data
-        for layer in self.layers:
-            output = layer.forward(output)
-        return output
-
-    def cost(self, X=None, y=None):
-        assert self.is_fitted, 'Model must be fit before predicting'
-        X = X if X is not None else self.dataset.X
-        y = y if y is not None else self.dataset.y
-        output = self.predict(X)
-        return self.loss(y, output)
-
-
-
 
 # im2col.py para pasta util
 # class Pooling2D
--- src/si/util/util.py
+++ src/si/util/util.py
@@ -64,16 +64,27 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 def to_categorical(y, num_classes=None, dtype='float32'):
+    '''
+    One-hot encoded vector (1 meaning TRUE, 0 meaning FALSE)
+    a vector which has integers that represent different categories,
+    can be converted into a matrix which has binary values
+    number of rows equal to the length of the input vector and
+    number of columns equal to the number of classes/categories.
+    each integer value is represented as a binary vector that is
+    all zero values except the index of the integer, which is marked with a 1. ([1, 0, 0] = class 1
+                                                                                [0, 1, 0] = class 2
+                                                                                [0, 0, 1] = class 3)
+    '''
     y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
+    input_shape = y.shape     #(no linhas, no colunas). shape[-1] -> no colunas. len(shape) -> no elementos da dimensão (2)
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1: #se y for 1 coluna (no linhas, 1)
+        input_shape = tuple(input_shape[:-1]) #dá um tuplo apenas com o no de linhas
+    y = y.ravel() #se tiver mais de uma coluna, flatten array (fica apenas uma coluna)
+    if not num_classes: #se num_classes=None
+        num_classes = np.max(y) + 1 #valor máximo de y + 1
+    n = y.shape[0] #no de linhas
+    categorical = np.zeros((n, num_classes), dtype=dtype) #array com n linhas e no de colunas igual ao no de categorias, preenchido a zeros
+    categorical[np.arange(n), y] = 1 #np.arange->array com valores até n-1. vai pôr os 1's
     output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
+    categorical = np.reshape(categorical, output_shape) #numpy.reshape(array, newshape)
     return categorical
