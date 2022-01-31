f3bc1610d51191b76da6c34ef3dc7f3b1dcc6a14
inestm28 <pg44378@alunos.uminho.pt>
2022-01-31 09:56:58 +0000

    Naive Bayes

--- src/si/supervised/__init__.py
+++ src/si/supervised/__init__.py
@@ -1,7 +1,8 @@
 from .model import Model
+from .nb import CategoricalNB
 from .knn import KNN
 from .linreg import LinearRegression, LinearRegressionReg
 from .logreg import LogisticRegression, LogisticRegressionReg
 from .dt import DecisionTree
 from .ensemble import Ensemble, majority, average
-from .nn import Layer, Dense, Activation, NN, Flatten, Conv2D
+#from .nn import Layer, Dense, Activation, NN
